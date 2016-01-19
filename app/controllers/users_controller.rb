a5e472eebb0d4d8cdd5ddccb326656e846e883fa
Ahmed Fayed <ahmedm.fayed@gmail.com>
2016-01-19 07:21:31 +0000

    Filled modal with user info in oil change card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :validate_user
   
-  def index
-    @users = User.includes(:profile)
-  end
-  
   def show
-    @user = User.find( params[:id] )
+    @cars = @user.cars.all.order("created_at") 
+    @user = current_user
+    @car = Car.find_by_id(params[:id])
   end
   
 end
