afdb8467d90e26de7da1c22789e7fa9978b8bd28
Ahmed Fayed <ahmedm.fayed@gmail.com>
2016-02-25 01:59:43 -0500

    Main page changes including color changes to the theme

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-    # def index
-    #     @ignore  = true
-    #     if user_signed_in?
-    #         redirect_to  new_user_order_path(current_user)
-    #     end
-    # end
+    def index
+        @ignore  = true
+        if user_signed_in?
+            redirect_to  new_user_order_path(current_user)
+        end
+    end
 end
