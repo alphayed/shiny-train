8a41299c261219e15162c1a1867952a8138d933c
Ahmed Fayed <ahmedm.fayed@gmail.com>
2016-02-29 21:40:35 -0500

    Fix for order buttons not working

--- app/assets/javascripts/side_nav.js
+++ app/assets/javascripts/side_nav.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 });
 });
 
-// Fixed double tap issue on ios devices
+// Fix double tap issue on ios devices
 $(document).ready(function() {
-  $('a').on('click touchend', function(e) {
+  $('a').on('touch', function(e) {
     var el = $(this);
     var link = el.attr('href');
     window.location = link;
