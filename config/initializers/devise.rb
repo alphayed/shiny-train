f9246890dcfd62931d95751c383b00c3d34c900f
Ahmed Fayed <ahmedm.fayed@gmail.com>
2016-03-03 19:09:12 -0500

    Figaro integration with devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '3cb648b7252b40d3f3205e1bf2bbea2e0ac7be973ae14ee3d0fc7e6abcda120c734445072245018dd2ac612a717f3af226cf22cb06397eea21d52e95f12e4cfc'
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
